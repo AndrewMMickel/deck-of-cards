0eb4aaf756071cff50456dd67ab0f8d748774f8b
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-05-06 11:38:27 -0700

    fixed coding to display deck correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
-$(document).ready(function() {
-    $('#Deck form').submit(function(event) {
-    var cards = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
-    var suits = ['clubs', 'spades', 'diamonds', 'hearts']
-    var fullcard = [];
-    cards.forEach(card); {
-        fullcard.push(cards + suits)}
+$(document).ready(function () {
+    $('form#Deck').submit(function (event) {
+        var cards = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+        var suits = ['clubs', 'spades', 'diamonds', 'hearts'];
+        var fullcard = [];
+        suits.forEach(function (suit) {
+            cards.forEach(function (card) {
+                fullcard.push(card + " " + suit)
+            });
+        });
+        fullcard.forEach(function (suit) {
+            $("ul").append("<li>" + suit + "</li>")
+        });
+        event.preventDefault();
     });
-
-    alert(fullcard)
 });
