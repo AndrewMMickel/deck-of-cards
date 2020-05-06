3ab61da9a43a03aa6ac333cdb0533a8e31f52914
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-05-06 10:09:26 -0700

    created html and javascript code

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#Deck form').submit(function(event) {
+    var cards = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+    var suits = ['clubs', 'spades', 'diamonds', 'hearts']
+    cards.forEach(card); {
+        }
+    });
+});
