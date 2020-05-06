08b150873d40afc605338e7520edc40a9077163b
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-05-06 10:41:36 -0700

    added bootstraps and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@ $(document).ready(function() {
     $('#Deck form').submit(function(event) {
     var cards = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
     var suits = ['clubs', 'spades', 'diamonds', 'hearts']
+    var fullcard = [];
     cards.forEach(card); {
-        }
+        fullcard.push(cards + suits)}
     });
+
+    alert(fullcard)
 });
